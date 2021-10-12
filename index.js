33a7ca1f41d6bd1a784dd281726e773fc832f566
Zohaib <zohaibmanzoor1700@gmail.com>
2021-10-12 14:22:57 +0000

    Completed 3 Questions

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
 //1
-function sumOfThreeAndFive(){
-  
+const sumOfThreeAndFive = () => {
+    let sum = 0;
+    for (let i = 0; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i
+        }
+    }
+    return sum
 }
 
-function greaterNum(){
-  
+//console.log(sumOfThreeAndFive())
+
+
+
+const greaterNum = (x, y) => {
+    if (x > y) {
+        return x
+    }
+    else if (x < y) {
+        return y
+    }
+    else if (x === y) {
+        return "both integers are equal"
+    }
+    else
+        return null;
 }
 
-function oddAndEvenInFifteen(){
-  
+//console.log(greaterNum("10", 10))
+
+
+
+const oddAndEvenInFifteen = () => {
+    for (let i = 0; i <= 15; i++) {
+        if (i % 2 === 0) {
+            console.log(`${i} is even`)
+        }
+        else {
+            console.log(`${i} is odd`)
+        }
+    }
 }
 
-function sortThreeNums(){
-  
-}
+//oddAndEvenInFifteen()
 
 
-
-
-
-
+// const sortThreeNums = (x, y, z) => {
+//     if (x > y && x > z) {
+//         console.log(x)
+//     }
+//     if (y > x && y > z){
+//         console.log(y)
+//     }
+//     if (z > y && z > x) {
+//         console.log(x)
+//     }
+// }
